e4fa6d7846c8c29aca150f954b298f3a22726f57
openminddev <147775420+openminddev@users.noreply.github.com>
2026-07-02 22:40:11 -0700

    sync: replace isaac_sim from OM1-ros2-sdk@fa47b298b22fe2140b35608252d7c744f60a797f (#19)

--- isaac_sim/run.py
+++ isaac_sim/run.py
@@ -739,7 +739,10 @@ def main():
         if human_runner is not None:
             human_runner.register_physics_callback()
         agent_runner.register_physics_callback()
-        people_runner.register_physics_callback(runner._world)
+        people_runner.register_physics_callback(
+            runner._world,
+            robot_pose_fn=lambda: runner._robot.robot.get_world_pose()[0],
+        )
         simulation_app.update()
         runner.setup_ros()
         simulation_app.update()
